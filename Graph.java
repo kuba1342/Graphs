fe168232b35e7cc4b8b64174a378a28e200a1b7c
kuba1342 <konres44@op.pl>
2019-05-31 12:12:23 +0200

    Add files via upload

---
+++ Graph.java
@@ -0,0 +1,39 @@
+public class Graph {
+
+    private final int V;    // number of vertices
+    private int E;          // number of edges
+    private Bag<Integer>[] adj; // adjacency lists
+
+    public Graph(int V) {
+        this.V = V;
+        this.E = 0;
+        adj = (Bag<Integer>[]) new Bag[V];  // Create array of lists
+        for (int v = 0; v < V; v++)         // Initialize all lists
+            adj[v] = new Bag<Integer>();    // to empty
+    }
+
+    public Graph(In in) {
+        this(in.readInt());     // Read V and construct this graph
+        int E = in.readInt();   // Read E
+        for (int i = 0; i < E; i++) {
+            // Add an edge
+            int v = in.readInt();   // Read a vertex
+            int w = in.readInt();   // read another vertex
+            addEdge(v, w);          // and add edge connecting them
+        }
+    }
+
+    public int V() { return V; }
+    public int E() { return E; }
+
+    public void addEdge(int v, int w) {
+        adj[v].add(w);
+        adj[w].add(v);
+        E++;
+    }
+
+    public Iterable<Integer> adj(int v) {
+        return adj[v];
+    }
+
+}
